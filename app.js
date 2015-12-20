3176e53d43ba2f9c8004bf14da86f394124c1e8d
marc.autran@laposte.net <yeeman>
2015-12-21 00:55:11 +0100

    classe + insert from web

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+'use strict';
+var app = require('express')();
+var bodyParser = require('body-parser');
 var Sequelize = require('sequelize');
+var Personne = require('./Personne');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// sequelize initialization
 var sequelize = new Sequelize('test', 'postgres', 'root', {
   host: 'localhost',
   dialect: 'postgres',
@@ -9,22 +18,47 @@ var sequelize = new Sequelize('test', 'postgres', 'root', {
   },
 });
 
-var Personne = sequelize.define('personne', {
-  nom: {
-    type: Sequelize.STRING,
-  },
-  prenom: {
-    type: Sequelize.STRING
-  }
+// model definition
+var personnes = sequelize.define("personnes", {
+    nom: Sequelize.STRING,
+    prenom: Sequelize.STRING
 });
 
-Personne.sync().then(function () {
-  return Personne.create({
-    nom: 'John',
-    prenom: 'Hancock'
-  });
+var creerPersonne = function(prenom, nom){
+	return (new Personne(prenom, nom));
+}
+
+var pers = creerPersonne("marc", "AUTRAN");
+
+var insertPersonne = function(pers){
+	personnes.sync().then(function(err) {
+		// insert new user
+		personnes.create({
+			nom: pers.nom,
+			prenom: pers.prenom
+		})
+	});
+} 
+
+//personnes.destroy({where: {nom: "bob"}}).then(function(arg){
+//		console.log(arg);
+//});
+
+//personnes.find({where: { nom: 'AUTRAN' }}).then(function(user) {
+//            console.log('Hello ' + user.nom + '!');
+//            console.log('All attributes of user:', user.prenom);
+//        });
+
+app.get("/", function(req, res) {
+    res.render('index.ejs', {nom: pers.nom, prenom: pers.prenom});
+})
+.post('/ajouter', function(req, res) {
+    if (req.body.nom != '' && req.body.prenom != '') 
+	{
+		pers = creerPersonne(req.body.prenom, req.body.nom);
+		insertPersonne(pers);
+    }
+    res.redirect('/');
 });
 
-Personne.findOne().then(function (user) {
-    console.log(user.nom);
-});
+app.listen( 5000);
